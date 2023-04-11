3746ae64d27714635683a492c46c62028a5ddd69
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-04-11 14:47:28 -0600

    Declare operator functions

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = function addFn(num1, num2) { num1 + num2;}
+
+const subtract = function subtractFn(num1, num2) { num1 - num2;}
+
+const multiply = function multiplyFn(num1, num2) { num1 * num2;}
+
+const divide = function addFn(num1, num2) { num1 / num2;}
+
+let firstNum;
+let operator;
+let secondNum;
+
+const operate = function operate(firstNum, secondNum, operator) {
+    // let result = operator(firstNum, secondNum)
+}
