1ef059ef4ba6ebe0724076c5c5e34841105ba459
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-04-17 17:22:31 -0600

    Add operate function.

--- script.js
+++ script.js
@@ -1,45 +1,172 @@
 // operator functions
-function addFn(num1, num2) { num1 + num2;}
+function addFn(num1, num2) { return num1 + num2;}
 function subtractFn(num1, num2) { num1 - num2;}
 function multiplyFn(num1, num2) { num1 * num2;}
 function divideFn(num1, num2) { num1 / num2;}
 
-const operate = function operate(num1, num2, operator) {
-}
 
+// elements
 const calculator = document.getElementById('calculator')
 const display = document.getElementById('display')
 const displayUpper = document.getElementById('display_upper');
 const displayLower = document.getElementById('display_lower');
 const clearBtn = document.getElementById('clear')
 
-const add = document.getElementById('add');
-add.addEventListener('click', addFn);
+const addBtn = document.getElementById('add');
+addBtn.addEventListener('click', addFn);
 
-const subtract = document.getElementById('subtract');
-subtract.addEventListener('click', subtractFn);
+const subtractBtn = document.getElementById('subtract');
+subtractBtn.addEventListener('click', subtractFn);
 
-const multiply = document.getElementById('multiply');
-multiply.addEventListener('click', multiplyFn);
+const multiplyBtn = document.getElementById('multiply');
+multiplyBtn.addEventListener('click', multiplyFn);
 
-const divide = document.getElementById('divide');
-divide.addEventListener('click', divideFn)
+const divideBtn = document.getElementById('divide');
+divideBtn.addEventListener('click', divideFn)
 
 
-const numBtn = document.querySelectorAll('.operand');
+const operandBtns = document.querySelectorAll('.operand');
+const specialBtns = document.querySelectorAll('.special');
+const operatorBtns = document.querySelectorAll('.operator');
+const equalsBtn = document.getElementById('equals');
 
-// iterate through each number button
-numBtn.forEach((button) => {
+// CALCULATOR LOGIC
+
+const clickedNum1 = []
+const clickedNum2 = []
+let num1;
+let num2;
+let operator;
+let result;
+
+
+// store operands
+operandBtns.forEach((button) => {
   button.addEventListener('click', displayClickedNum);
 });
 
-let clickedNum = []
 
 function displayClickedNum(event) {
-    let clickVal = parseInt(event.target.textContent)
-    clickedNum.push(clickVal);
-    displayLower.innerText = clickedNum;
-    return clickedNum;
+  if (operator === undefined) {
+    let clickVal = event.target.textContent;
+    clickedNum1.push(clickVal);
+    num1 = clickedNum1.join('');
+    num1 = parseInt(num1);
+    displayLower.innerText = num1;
+    console.log(num1);
+  }
+  if (operator !== undefined) {
+    let clickVal = event.target.textContent;
+    clickedNum2.push(clickVal);
+    num2 = clickedNum2.join('');
+    num2 = parseInt(num2);
+    displayLower.innerText = num2;
+    console.log(num2);
+  }
 }
 
+operatorBtns.forEach((button) => {
+  button.addEventListener('click', storeOperator);
+});
 
+function storeOperator(event) {
+  if (operator === undefined) {
+    operator = event.target.id
+    console.log(operator);
+  }
+}
+
+function operate() {
+  if (operator === 'add') {
+    result = num1 + num2;
+      displayLower.innerText = result;
+  }
+
+}
+
+equalsBtn.addEventListener('click', operate)
+
+
+/* 
+() => {
+  if (operator === 'add') {
+    result = num1 + num2;
+      console.log(result);
+    }
+});
+*/
+
+
+
+/*
+   switch (operator) {
+      case 'add':
+        console.log('plus');
+        break;
+      case 'subtract':
+        console.log('minus');
+        break;
+      case 'multiply':
+        console.log('times');
+        break;
+      case 'divide':
+        console.log('division');
+        break;
+    }
+  }
+} */
+
+
+/*
+function displayClickedNum(event) {
+  if (clickedNum.length <= 12) { //change from 8 after removing commas
+    let clickVal = event.target.textContent;
+    clickedNum.push(clickVal);
+    const newArr = clickedNum.join('');
+    displayLower.innerText = newArr;
+    return newArr;
+  }
+}
+
+*/
+
+
+// END OF LOGIC
+
+
+// styling buttons
+
+
+operandBtns.forEach((operandBtn) => {
+  operandBtn.addEventListener('mousedown', () => {
+    operandBtn.style.backgroundColor = '#FFDBE5';
+  })
+  operandBtn.addEventListener('mouseup', () => {
+    operandBtn.style.backgroundColor = '#FF9EBB';
+  })
+});
+
+specialBtns.forEach((specialBtn) => {
+  specialBtn.addEventListener('mousedown', () => {
+    specialBtn.style.backgroundColor = '#AECEEA';
+  })
+  specialBtn.addEventListener('mouseup', () => {
+    specialBtn.style.backgroundColor = '#79ADDC';
+  })
+});
+
+operatorBtns.forEach((operatorBtn) => {
+  operatorBtn.addEventListener('mousedown', () => {
+    operatorBtn.style.backgroundColor = '#FFE4AD';
+  })
+  operatorBtn.addEventListener('mouseup', () => {
+    operatorBtn.style.backgroundColor = '#FFD275';
+  })
+});
+
+equals.addEventListener('mousedown', () => {
+    equals.style.backgroundColor = '#FFE4AD';
+  })
+  equals.addEventListener('mouseup', () => {
+    equals.style.backgroundColor = '#FFD275';
+  });
