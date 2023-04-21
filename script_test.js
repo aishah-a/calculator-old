ab2bc5a3c5db4a58f1879ffb0b1abd85f5c2fb0c
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-04-20 21:05:26 -0600

    Add test script file

---
+++ script_test.js
@@ -0,0 +1,156 @@
+// elements
+const calculator = document.getElementById('calculator')
+const display = document.getElementById('display')
+const displayUpper = document.getElementById('display_upper');
+const displayLower = document.getElementById('display_lower');
+const clearBtn = document.getElementById('clear');
+const decimalBtn = document.getElementById('decimal');
+
+const addBtn = document.getElementById('add');
+const subtractBtn = document.getElementById('subtract');
+const multiplyBtn = document.getElementById('multiply');
+const divideBtn = document.getElementById('divide');
+
+const operandBtns = document.querySelectorAll('.operand');
+const specialBtns = document.querySelectorAll('.special');
+const operatorBtns = document.querySelectorAll('.operator');
+const equalsBtn = document.getElementById('equals');
+
+// CALCULATOR LOGIC
+
+let clickedNum1 = [];
+let clickedNum2 = [];
+let num1 = null;
+let num2 = null;
+let chosenOperator = [];
+let operator = null;
+let result = null;
+
+
+// store operands
+operandBtns.forEach((button) => {
+  button.addEventListener('click', storeClickedNum);
+});
+
+function storeClickedNum(event) {
+  if (clickedNum1.length <= 12 && clickedNum2.length <= 12) {
+    // initial operation
+    if (operator === null) {
+      let clickVal = event.target.textContent;
+      clickedNum1.push(clickVal);
+      num1 = clickedNum1.join(''); // join clicked nums in array
+      num1 = parseInt(num1); // turn string into num
+      displayLower.innerText = num1;
+      console.log('num1 is ' + num1);
+    } else if (operator !== null && num1 !== null) {
+      let clickVal = event.target.textContent;
+      clickedNum2.push(clickVal);
+      num2 = clickedNum2.join(''); // join clicked nums in array
+      num2 = parseInt(num2); // turn string into num
+      displayLower.innerText = num2;
+      console.log('num2 is ' + num2);
+    } if (result !== null && num2 !== null) {
+      num1 = result;
+    }
+  } else {
+    displayLower.style.fontSize = '30px';
+    displayLower.innerText = 'Number is too long!'
+  }
+}
+
+
+operatorBtns.forEach((button) => {
+  button.addEventListener('click', storeOperator);
+});
+
+function storeOperator(event) {
+  // inital operation
+  if (operator === null) {
+    chosenOperator.push(event.target.id);
+    operator = chosenOperator.toString();
+    console.log(operator);
+    return operator;
+  } // second operation
+  if (result !== null || num2 !== null) {
+    chosenOperator.pop();
+    chosenOperator.push(event.target.id);
+    operator = chosenOperator.toString();
+    console.log('operator is ' + operator);
+    operate();
+    return operator;
+  }
+}
+
+
+function operate() {
+  if (operator === 'add') {
+    result = num1 + num2;
+  } else if (operator === 'subtract') {
+    result = num1 - num2;
+  } else if (operator === 'multiply') {
+    result = num1 * num2;
+  } else if (operator === 'divide') {
+    result = num1 / num2;
+  }
+  displayLower.innerText = result;
+  console.log('equals');
+  console.log('result is ' + result);
+  clickedNum1.length = 0;
+  clickedNum2.length = 0;
+}
+
+
+equalsBtn.addEventListener('click', operate)
+
+/*
+
+// END OF LOGIC
+
+
+
+clearBtn.addEventListener('click', clearFn)
+
+function clearFn() {
+  num2 = null;
+
+  displayLower.innerText = 0;
+}
+
+*/
+
+// styling buttons
+
+
+operandBtns.forEach((operandBtn) => {
+  operandBtn.addEventListener('mousedown', () => {
+    operandBtn.style.backgroundColor = '#FFDBE5';
+  })
+  operandBtn.addEventListener('mouseup', () => {
+    operandBtn.style.backgroundColor = '#FF9EBB';
+  })
+});
+
+specialBtns.forEach((specialBtn) => {
+  specialBtn.addEventListener('mousedown', () => {
+    specialBtn.style.backgroundColor = '#AECEEA';
+  })
+  specialBtn.addEventListener('mouseup', () => {
+    specialBtn.style.backgroundColor = '#79ADDC';
+  })
+});
+
+operatorBtns.forEach((operatorBtn) => {
+  operatorBtn.addEventListener('mousedown', () => {
+    operatorBtn.style.backgroundColor = '#FFE4AD';
+  })
+  operatorBtn.addEventListener('mouseup', () => {
+    operatorBtn.style.backgroundColor = '#FFD275';
+  })
+});
+
+equals.addEventListener('mousedown', () => {
+    equals.style.backgroundColor = '#FFE4AD';
+  })
+  equals.addEventListener('mouseup', () => {
+    equals.style.backgroundColor = '#FFD275';
+  });
